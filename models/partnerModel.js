a4c0e82d5cae62118a5679b968086aeff17aa40e
Tim Miller <xxtimtimxx@gmail.com>
2022-07-25 16:50:35 -0400

    Added slugs to partner model

--- models/partnerModel.js
+++ models/partnerModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const partnerSchema = new mongoose.Schema({
   name: {
@@ -19,19 +20,28 @@ const partnerSchema = new mongoose.Schema({
   businessHours: {
     open: {
       type: String,
-      required: [
-        true,
-        'Business hours start time must be specified in this format HH:MM',
-      ],
+      required: [true, 'Business hours start time must be specified in this format HH:MM'],
     },
     close: {
       type: String,
-      required: [
-        true,
-        'Business hours end time must be specified in this format HH:MM',
-      ],
+      required: [true, 'Business hours end time must be specified in this format HH:MM'],
     },
   },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+// Create slug from name. Runs before any validation
+partnerSchema.pre('validate', function (next) {
+  if (this.name) {
+    // Sets slug to lowercase and gets rid of any characters that don't fit in a URL such as ':'
+    this.slug = slugify(this.name, { lower: true, strict: true });
+  }
+
+  next();
 });
 
 const Partner = mongoose.model('Partner', partnerSchema);
